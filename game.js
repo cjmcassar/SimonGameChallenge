7a6520bb9279d8c0e27630cf7fa1da305d5f3156
Christopher-John Cassar <christopher-john.cassar.16@ucl.ac.uk>
2020-10-12 17:34:25 -0400

    - Create a new pattern via js

--- game.js
+++ game.js
@@ -1,1 +1,15 @@
-alert("woah");
+
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+
+function nextSequence()
+{
+  randomNumber = Math.floor(Math.random()*4);
+  var randomChosenColour = buttonColours[randomNumber];
+
+  gamePattern.push(randomChosenColour);
+  //console.log(randomChosenColour);
+  //return randomChosenColour;
+}
+
+nextSequence();
